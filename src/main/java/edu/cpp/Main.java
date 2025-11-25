b134f04d6f9b1d715ecc90a721481247acf145c2
Clarence Ballensky <alballensky@cpp.edu>
2025-11-24 22:35:51 -0800

    added computeOptimalCosts method

--- src/main/java/edu/cpp/Main.java
+++ src/main/java/edu/cpp/Main.java
@@ -80,4 +80,58 @@ public class Main {
 
         return null; //in event of an error
     }
+
+    /**
+     * Method: computeOptimalCosts
+     * Purpose: use dynamic programming to compute the optimal canoe route, with the optimal route defined as the
+     *          cheapest route
+     * @param costMatrix cost of traveling from any post to any other post
+     * @param optimalMatrix stores the minimum cost from any post to any other post
+     * @param nextMatrix stores the sequence of canoe rentals in the optimal order
+     * @param numberTradingPosts the number of trading posts on the user-provided canoe route
+     */
+    public static void computeOptimalCosts(int[][] costMatrix, int[][] optimalMatrix, int[][] nextMatrix,
+                                           int numberTradingPosts)
+    {
+        //set the cost of travelling from a post to itself to 0
+        for (int i = 0; i < numberTradingPosts; i++) {
+            optimalMatrix[i][i] = 0;
+        }
+
+        /*
+         * Length is the difference between the end post and the start post
+         *         for example,
+         *                  when length = 1, compute optimalMatrix[0][1], optimalMatrix[1][2], optimalMatrix[2][3], ..
+         *                  when length = 2, compute optimalMatrix[0][2], optimalMatrix[1][3], optimalMatrix[2][4], ...
+         *
+         * This ensures that all smaller subproblems have already been computed.
+         */
+        for (int length = 1; length < numberTradingPosts - 1; length++) {
+            for (int startPost = 0; startPost < numberTradingPosts - length - 1; startPost++) {
+                int endPost = startPost + length; //endPost is length distance away from startPost
+
+                //start by assuming the best solution is a direct rental from the startPost to the endPost
+                int bestCost =  costMatrix[startPost][endPost]; //best cost for travelling between two posts
+                int bestNext = endPost; //bestNext is the next post you should go to after startPost to minimize cost
+
+                //try all intermediate stops where startPost < intermediatePost < endPost
+                for (int intermediatePost = startPost + 1; intermediatePost < endPost -1; intermediatePost++) {
+                    //cost to rent a canoe from startPost to intermediatePost + optimal cost from k to j
+                    //handles any other necessary intermediate stops recursively
+                    int cost = costMatrix[startPost][intermediatePost] + optimalMatrix[intermediatePost][endPost];
+
+                    //if stopping at the intermediatePost yields a cheaper route (this is desirable),
+                    //update bestCost and bestNext
+                    if (cost < bestCost) {
+                        bestCost = cost;
+                        bestNext = intermediatePost;
+                    }
+                }
+
+                //store the results
+                optimalMatrix[startPost][endPost] = bestCost; //best minimal cost from startPost to stopPost
+                nextMatrix[startPost][endPost] = bestNext; //first stop after startPost on the optimal route
+            }
+        }
+    }
 }
